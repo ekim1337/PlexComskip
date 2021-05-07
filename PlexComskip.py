4cfdd56079a4ba50923429cbc54c5204c0d6e117
Darren Crotchett <dcrotchett@mdot.ms.gov>
2021-05-07 11:14:11 -0500

    Refactored for Python 3 - Added mp4 export option which involved PlexComskip.conf changes

--- PlexComskip.py
+++ PlexComskip.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import ConfigParser
+import configparser
 import logging
 import os
 import shutil
@@ -11,14 +11,15 @@ import time
 import uuid
 
 # Config stuff.
-config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'PlexComskip.conf')
+pwd = os.path.dirname(os.path.realpath(__file__))
+config_file_path = os.path.join(pwd, 'PlexComskip.conf')
 if not os.path.exists(config_file_path):
-  print 'Config file not found: %s' % config_file_path
-  print 'Make a copy of PlexConfig.conf.example named PlexConfig.conf, modify as necessary, and place in the same directory as this script.'
+  print(f'Config file not found: {config_file_path}')
+  print('Make a copy of PlexConfig.conf.example named PlexConfig.conf, modify as necessary, and place in the same directory as this script.')
   sys.exit(1)
 
-config = ConfigParser.SafeConfigParser({
-  'comskip-ini-path' : os.path.join(os.path.dirname(os.path.realpath(__file__)), 'comskip.ini'),
+config = configparser.ConfigParser({
+  'comskip-ini-path' : os.path.join(pwd, 'comskip.ini'),
   'temp-root' : tempfile.gettempdir(),
   'comskip-root' : tempfile.gettempdir(),
   'nice-level' : '0'
@@ -36,6 +37,8 @@ COPY_ORIGINAL = config.getboolean('File Manipulation', 'copy-original')
 SAVE_ALWAYS = config.getboolean('File Manipulation', 'save-always')
 SAVE_FORENSICS = config.getboolean('File Manipulation', 'save-forensics')
 NICE_LEVEL = config.get('Helper Apps', 'nice-level')
+CONVERT = config.getboolean('File Manipulation', 'convert-to-mp4')
+KEEP_ORIGINAL = config.getboolean('File Manipulation', 'keep-original')
 
 # Exit states
 CONVERSION_SUCCESS = 0
@@ -46,7 +49,7 @@ COMSKIP_FAILED = 4
 
 # Logging.
 session_uuid = str(uuid.uuid4())
-fmt = '%%(asctime)-15s [%s] %%(message)s' % session_uuid[:6]
+fmt = f'%%(asctime)-15s [{session_uuid[:6]}] %%(message)s'
 if not os.path.exists(os.path.dirname(LOG_FILE_PATH)):
   os.makedirs(os.path.dirname(LOG_FILE_PATH))
 logging.basicConfig(level=logging.INFO, format=fmt, filename=LOG_FILE_PATH)
@@ -62,27 +65,35 @@ def sizeof_fmt(num, suffix='B'):
 
   for unit in ['','K','M','G','T','P','E','Z']:
     if abs(num) < 1024.0:
-      return "%3.1f%s%s" % (num, unit, suffix)
+        return f"{num:3.1f}{unit}{suffix}"
     num /= 1024.0
-  return "%.1f%s%s" % (num, 'Y', suffix)
+  return f"{num:.1f}Y{suffix}"
 
 if len(sys.argv) < 2:
-  print 'Usage: PlexComskip.py input-file.mkv'
+  print('Usage: PlexComskip.py input-file.mkv')
   sys.exit(1)
 
 # Clean up after ourselves and exit.
-def cleanup_and_exit(temp_dir, keep_temp=False, exit_code=CONVERSION_SUCCESS):
+def cleanup_and_exit(temp_dir, keep_temp=False, exit_code=CONVERSION_SUCCESS, **kwargs):
   if keep_temp:
-    logging.info('Leaving temp files in: %s, %s' % (temp_dir, comskip_out))
+    logging.info(f'Leaving temp files in: {temp_dir}, {comskip_out}')
   else:
     try:
       os.chdir(os.path.expanduser('~'))  # Get out of the temp dir before we nuke it (causes issues on NTFS)
       shutil.rmtree(temp_dir)
       if temp_dir != comskip_out:
         shutil.rmtree(comskip_out)
-    except Exception, e:
-      logging.error('Problem whacking temp dirs: %s, %s' % (temp_dir, comskip_out))
+    except Exception as e:
+      logging.error('Problem whacking temp dirs: {temp_dir}, {comskip_out}')
       logging.error(str(e))
+      exit_code=EXCEPTION_HANDLED
+  if not KEEP_ORIGINAL:
+    try:
+      orig_file = kwargs.get('original_file')
+      os.remove(orig_file)
+      logging.info(f'{orig_file} removed per the setting in the PlexComskip.conf file "keep-original: False"')
+    except:
+      logging.error(f'Unable to remove file: {orig_file}')
       exit_code=EXCEPTION_HANDLED
 
   # Exit cleanly.
@@ -90,11 +101,11 @@ def cleanup_and_exit(temp_dir, keep_temp=False, exit_code=CONVERSION_SUCCESS):
   sys.exit(exit_code)
 
 # If we're in a git repo, let's see if we can report our sha.
-logging.info('PlexComskip got invoked from %s' % os.path.realpath(__file__))
+logging.info(f'PlexComskip got invoked from {pwd}')
 try:
   git_sha = subprocess.check_output('git rev-parse --short HEAD', shell=True)
   if git_sha:
-    logging.info('Using version: %s' % git_sha.strip())
+    logging.info(f'Using version: {git_sha.strip()}')
 except: pass
 
 # Set our own nice level and tee up some args for subprocesses (unix-like OSes only).
@@ -105,12 +116,15 @@ if sys.platform != 'win32':
     if nice_int > 0:
       os.nice(nice_int)
       NICE_ARGS = ['nice', '-n', str(nice_int)]
-  except Exception, e:
-    logging.error('Couldn\'t set nice level to %s: %s' % (NICE_LEVEL, e))
+  except Exception as e:
+    logging.error(f'Couldn\'t set nice level to {NICE_LEVEL}: {e}')
 
 # On to the actual work.
 try:
   video_path = os.path.abspath(sys.argv[1])
+  if not os.path.exists(video_path):
+      logging.error(f'{video_path} does not exist.  Please check the input file.')
+      raise Exception('input file does not exist')
   temp_dir = os.path.join(TEMP_ROOT, session_uuid)
   comskip_out = os.path.join(COMSKIP_ROOT, session_uuid)
   os.makedirs(temp_dir)
@@ -118,42 +132,42 @@ try:
     os.makedirs(comskip_out)
   os.chdir(temp_dir)
 
-  logging.info('Using session ID: %s' % session_uuid)
-  logging.info('Using temp dir: %s' % temp_dir)
-  logging.info('Using comskip dir: %s' % comskip_out)
-  logging.info('Using input file: %s' % video_path)
+  logging.info(f'Using session ID: {session_uuid}')
+  logging.info(f'Using temp dir: {temp_dir}')
+  logging.info(f'Using comskip dir: {comskip_out}')
+  logging.info(f'Using input file: {video_path}')
 
   output_video_dir = os.path.dirname(video_path)
   if sys.argv[2:]:
-    logging.info('Output will be put in: %s' % sys.argv[2])
+    logging.info(f'Output will be put in: {sys.argv[2]}')
     output_video_dir = os.path.dirname(sys.argv[2])
 
   video_basename = os.path.basename(video_path)
   video_name, video_ext = os.path.splitext(video_basename)
 
-except Exception, e:
-  logging.error('Something went wrong setting up temp paths and working files: %s' % e)
+except Exception as e:
+  logging.error(f'Something went wrong setting up temp paths and working files: {e}')
   sys.exit(EXCEPTION_HANDLED)
 
 try:
   if COPY_ORIGINAL or SAVE_ALWAYS: 
-    temp_video_path = os.path.join(temp_dir, video_basename)
-    logging.info('Copying file to work on it: %s' % temp_video_path)
+    video_source_file = os.path.join(temp_dir, video_basename)
+    logging.info(f'Copying file to work on it: {video_source_file}')
     shutil.copy(video_path, temp_dir)
   else:
-    temp_video_path = video_path
+    video_source_file = video_path
 
   # Process with comskip.
-  cmd = NICE_ARGS + [COMSKIP_PATH, '--output', comskip_out, '--ini', COMSKIP_INI_PATH, temp_video_path]
-  logging.info('[comskip] Command: %s' % cmd)
+  cmd = NICE_ARGS + [COMSKIP_PATH, '--output', comskip_out, '--ini', COMSKIP_INI_PATH, video_source_file]
+  logging.info(f'[comskip] Command: {cmd}')
   comskip_status = subprocess.call(cmd)
   if comskip_status != 0:
-    logging.error('Comskip did not exit properly with code: %s' % comskip_status)
+    logging.error(f'Comskip did not exit properly with code: {comskip_status}')
     cleanup_and_exit(temp_dir, False, COMSKIP_FAILED)
     #raise Exception('Comskip did not exit properly')
 
-except Exception, e:
-  logging.error('Something went wrong during comskip analysis: %s' % e)
+except Exception as e:
+  logging.error(f'Something went wrong during comskip analysis: {e}')
   cleanup_and_exit(temp_dir, SAVE_ALWAYS or SAVE_FORENSICS, EXCEPTION_HANDLED)
 
 edl_file = os.path.join(comskip_out, video_name + '.edl')
@@ -171,73 +185,99 @@ try:
           logging.info('Start of file is junk, skipping this segment...')
         else:
           keep_segment = [float(prev_segment_end), float(start)]
-          logging.info('Keeping segment from %s to %s...' % (keep_segment[0], keep_segment[1]))
+          logging.info(f'Keeping segment from {keep_segment[0]} to {keep_segment[1]}...')
           segments.append(keep_segment)
         prev_segment_end = end
 
   # Write the final keep segment from the end of the last commercial break to the end of the file.
   keep_segment = [float(prev_segment_end), -1]
-  logging.info('Keeping segment from %s to the end of the file...' % prev_segment_end)
+  logging.info(f'Keeping segment from {float(prev_segment_end)} to the end of the file...')
   segments.append(keep_segment)
 
   segment_files = []
   segment_list_file_path = os.path.join(temp_dir, 'segments.txt')
   with open(segment_list_file_path, 'wb') as segment_list_file:
     for i, segment in enumerate(segments):
-      segment_name = 'segment-%s' % i
-      segment_file_name = '%s%s' % (segment_name, video_ext)
+      segment_name = f'segment-{i}'
+      segment_file_name = f'{segment_name}{video_ext}'
       if segment[1] == -1:
         duration_args = []
       else:
         duration_args = ['-t', str(segment[1] - segment[0])]
-      cmd = NICE_ARGS + [FFMPEG_PATH, '-i', temp_video_path, '-ss', str(segment[0])]
+      cmd = NICE_ARGS + [FFMPEG_PATH, '-i', video_source_file, '-ss', str(segment[0])]
       cmd.extend(duration_args)
-      cmd.extend(['-c', 'copy', segment_file_name])
-      logging.info('[ffmpeg] Command: %s' % cmd)
+      # cmd.extend(['-c', 'copy', segment_file_name])
+      cmd.extend([segment_file_name])
+      logging.info(f'[ffmpeg] Command: {cmd}')
       try:
         subprocess.call(cmd)
-      except Exception, e:
-        logging.error('Exception running ffmpeg: %s' % e)
+      except Exception as e:
+        logging.error(f'Exception running ffmpeg: {e}')
         cleanup_and_exit(temp_dir, SAVE_ALWAYS or SAVE_FORENSICS, EXCEPTION_HANDLED)
       
       # If the last drop segment ended at the end of the file, we will have written a zero-duration file.
       if os.path.exists(segment_file_name):
         if os.path.getsize(segment_file_name) < 1000:
-          logging.info('Last segment ran to the end of the file, not adding bogus segment %s for concatenation.' % (i + 1))
+          logging.info(f'Last segment ran to the end of the file, not adding bogus segment {(i+1)} for concatenation.')
           continue
 
         segment_files.append(segment_file_name)
-        segment_list_file.write('file %s\n' % segment_file_name)
+        segment_list_file.write(f'file {segment_file_name}\n'.encode('utf-8'))
 
-except Exception, e:
-  logging.error('Something went wrong during splitting: %s' % e)
+except Exception as e:
+  logging.error(f'Something went wrong during splitting: {e}'.encode('utf-8'))
   cleanup_and_exit(temp_dir, SAVE_ALWAYS or SAVE_FORENSICS, EXCEPTION_HANDLED)
 
-logging.info('Going to concatenate %s files from the segment list.' % len(segment_files))
+logging.info(f'Going to concatenate {len(segment_files)} files from the segment list.')
 try:
-  cmd = NICE_ARGS + [FFMPEG_PATH, '-y', '-f', 'concat', '-i', segment_list_file_path, '-c', 'copy', os.path.join(temp_dir, video_basename)]
-  logging.info('[ffmpeg] Command: %s' % cmd)
+  if CONVERT:
+    video_basename = os.path.splitext(video_basename)[0] + '.mp4'
+    cmd = NICE_ARGS + [FFMPEG_PATH, '-y', '-f', 'concat', '-i', segment_list_file_path, '-c:v', 'libx264', os.path.join(temp_dir, video_basename)]
+  else:
+    cmd = NICE_ARGS + [FFMPEG_PATH, '-y', '-f', 'concat', '-i', segment_list_file_path, '-c', 'copy', os.path.join(temp_dir, video_basename)]
+  logging.info(f'[ffmpeg] Command: {cmd}')
   subprocess.call(cmd)
 
-except Exception, e:
-  logging.error('Something went wrong during concatenation: %s' % e)
+except Exception as e:
+  logging.error(f'Something went wrong during concatenation: {e}')
   cleanup_and_exit(temp_dir, SAVE_ALWAYS or SAVE_FORENSICS, EXCEPTION_HANDLED)
 
-logging.info('Sanity checking our work...')
 try:
-  input_size = os.path.getsize(os.path.abspath(video_path))
-  output_size = os.path.getsize(os.path.abspath(os.path.join(temp_dir, video_basename)))
-  if input_size and 1.01 > float(output_size) / float(input_size) > 0.99:
-    logging.info('Output file size was too similar (doesn\'t look like we did much); we won\'t replace the original: %s -> %s' % (sizeof_fmt(input_size), sizeof_fmt(output_size)))
-    cleanup_and_exit(temp_dir, SAVE_ALWAYS, CONVERSION_DID_NOT_MODIFY_ORIGINAL)
-  elif input_size and 1.1 > float(output_size) / float(input_size) > 0.5:
-    logging.info('Output file size looked sane, we\'ll replace the original: %s -> %s' % (sizeof_fmt(input_size), sizeof_fmt(output_size)))
-    logging.info('Copying the output file into place: %s -> %s' % (video_basename, output_video_dir))
-    shutil.copy(os.path.join(temp_dir, video_basename), output_video_dir)
-    cleanup_and_exit(temp_dir, SAVE_ALWAYS)
+  if CONVERT or KEEP_ORIGINAL:
+    logging.info(f'Sanity check skipped when files are converted or original is saved.')
+    # convert will be creating a new file name so no need to preserve the original
+    if CONVERT:
+      logging.info(f'Copying the output file into place: {video_basename} -> {output_video_dir}')
+      shutil.copy(os.path.join(temp_dir, video_basename), output_video_dir)
+    # not converting so we will be losing the original, if we don't give the new file a new name
+    else:
+      new_video_basename, extension = os.path.splitext(video_basename)
+      new_video_basename += '.no-commercials' + extension
+      source = os.path.join(temp_dir, video_basename)
+      destination = os.path.join(output_video_dir, new_video_basename)
+      logging.info(f'Copying the output file into place: {source} -> {destination}')
+      shutil.copy(source, destination)
+
+    cleanup_and_exit(temp_dir, SAVE_ALWAYS, original_file=video_path)
   else:
-    logging.info('Output file size looked wonky (too big or too small); we won\'t replace the original: %s -> %s' % (sizeof_fmt(input_size), sizeof_fmt(output_size)))
-    cleanup_and_exit(temp_dir, SAVE_ALWAYS or SAVE_FORENSICS, CONVERSION_SANITY_CHECK_FAILED)
-except Exception, e:
-  logging.error('Something went wrong during sanity check: %s' % e)
+    logging.info('Sanity checking our work...')
+    input_size = os.path.getsize(os.path.abspath(video_path))
+    output_size = os.path.getsize(os.path.abspath(os.path.join(temp_dir, video_basename)))
+    if input_size and 1.01 > float(output_size) / float(input_size) > 0.99:
+      logging.info(f'Output file size was too similar (doesn\'t look like we did much); we won\'t replace the original: {sizeof_fmt(input_size)} -> {sizeof_fmt(output_size)}')
+      logging.info('too little difference - suspect problem - override KEEP_ORIGINAL')
+      KEEP_ORIGINAL = True
+      cleanup_and_exit(temp_dir, SAVE_ALWAYS, CONVERSION_DID_NOT_MODIFY_ORIGINAL)
+    elif input_size and 1.1 > float(output_size) / float(input_size) > 0.5:
+      logging.info(f'Output file size looked sane, we\'ll replace the original: {sizeof_fmt(input_size)} -> {sizeof_fmt(output_size)}')
+      logging.info(f'Copying the output file into place: {video_basename} -> {output_video_dir}')
+      shutil.copy(os.path.join(temp_dir, video_basename), output_video_dir)
+      cleanup_and_exit(temp_dir, SAVE_ALWAYS)
+    else:
+      logging.info(f'too much difference between before and after sizes- suspect problem - override KEEP_ORIGINAL')
+      KEEP_ORIGINAL = True
+      logging.info(f'Output file size looked wonky (too big or too small); we won\'t replace the original: {sizeof_fmt(input_size)} -> {sizeof_fmt(output_size)}')
+      cleanup_and_exit(temp_dir, SAVE_ALWAYS or SAVE_FORENSICS, CONVERSION_SANITY_CHECK_FAILED)
+except Exception as e:
+  logging.error(f'Something went wrong during sanity check: {e}')
   cleanup_and_exit(temp_dir, SAVE_ALWAYS or SAVE_FORENSICS, EXCEPTION_HANDLED)
